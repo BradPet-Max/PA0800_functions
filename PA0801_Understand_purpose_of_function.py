56f24a126e1807155d4e9f66d8c8ade46ff5d4e2
Peter Mavazhe <peterbmavavazhe@gmail.com>
2026-07-16 15:55:49 +0200

    Add initial implementation of function definition and usage examples

---
+++ PA0801_Understand_purpose_of_function.py
@@ -0,0 +1,21 @@
+#Question 1-In your own words, define a function.
+#A function is a reusable block of code that performs a specific task. 
+# It can be called whenever it is needed, which helps avoid writing the same code multiple times.
+
+#Question 2-Why is repeating code considered bad practice?
+#Repeating code increases the likelihood of errors, makes the code harder to maintain, 
+# and violates the principle of DRY (Don't Repeat Yourself).
+
+#Question 3-What are two advantages of using functions?
+#Two advantages of using functions are:
+#1. Code reusability: Functions can be called multiple times without having to rewrite the same code.
+#2. Easier maintenance: If a function needs to be updated, the change only needs to be made in one place.
+
+#Question 4 (Practical Thinking)
+def system_ready():
+    print("System Ready")
+
+system_ready()
+system_ready()
+system_ready()
+
